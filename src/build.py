cf5c9ad9f3d118a657447917a438c506cc2321f1
yury.badalyants <yury.badalyants@parrotanalytics.com>
2024-07-07 13:20:50 +1200

    Templating

--- src/build.py
+++ src/build.py
@@ -1,9 +1,26 @@
+from enum import Enum
+from functools import partial
+
 from jinja2.environment import Environment
 from jinja2.loaders import FileSystemLoader
 from content import texts
 
 
-def render():
+class Version(Enum):
+    CONSICE = "CONSICE"
+    DETAILED = "detailed"
+
+
+def text(concise: str, detailed: str, version: Version):
+    if version == Version.DETAILED:
+        return detailed
+    else:
+        return concise
+
+
+def render(version: Version = Version.DETAILED) -> str:
     env = Environment(loader=FileSystemLoader('.'))
+    env.globals['text'] = partial(text, version=version)
+    env.globals['version'] = version
     template = env.get_template('Template.html')
     return template.render(texts=texts)
