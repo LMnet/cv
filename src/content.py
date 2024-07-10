90006752ab905437c7fb5c8831bab61887002465
yury.badalyants <yury.badalyants@parrotanalytics.com>
2024-07-10 14:01:06 +1200

    Fixes

--- src/content.py
+++ src/content.py
@@ -9,11 +9,11 @@ class Link:
 
 talks = [
     Link("Intro to Scala.js",                               "https://www.youtube.com/watch?v=QTGtCTtIrHc"),
-    Link("Scala.js: When TypeScript is not enough",         "https://youtu.be/Ln8woWsIBoI?t=2h10m7s"),
+    Link("Scala.js: When TypeScript is Not Enough",         "https://youtu.be/Ln8woWsIBoI?t=2h10m7s"),
     Link("Metaprogramming in Scala",                        "https://www.youtube.com/watch?v=AbIasjx6yPA"),
-    Link("Integration testing of microservices with Scala", "https://www.youtube.com/watch?v=zE2sLcIeoZk"),
-    Link("Pure FP: why and how",                            "https://www.youtube.com/watch?v=pCtTBUeO41w"),
-    Link("Production-ready functional programming",         "https://youtu.be/wROJoMUHLck"),
+    Link("Integration Testing of Microservices with Scala", "https://www.youtube.com/watch?v=zE2sLcIeoZk"),
+    Link("Pure FP: Why and How",                            "https://www.youtube.com/watch?v=pCtTBUeO41w"),
+    Link("Production-Ready Functional Programming",         "https://youtu.be/wROJoMUHLck"),
 ]
 
 texts = ns(
